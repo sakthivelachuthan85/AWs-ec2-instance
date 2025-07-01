b6e653845e643d949af96c95d53b289183ea85b3
sakthivelachuthan85 <sakthivel.achuthan@outlook.com>
2025-07-01 20:11:40 +0530

    created ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+ 
+resource "aws_instance" "Sakthi-Server" {
+    ami = "ami-0efc43a4067fe9a3e"
+    instance_type = "t2.micro" 
+tags = {
+    Name = "Demo-Server"
+  } 
+}
